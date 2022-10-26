414f01b019eaa81cedff5fd9623804e9d8f5ed70
Adam Gausmann <adam@gaussian.dev>
2022-10-26 17:41:51 -0500

    Generalize firmware by computing an index into EEPROM.
    The index is calculated from the cell's own state and its neighbor states,
    which allows it to emulate any Life-like automaton and, more generally,
    any 2-state cellular automaton in which each cell's state is a function of its
    Moore neighborhood.

--- life-cell/firmware/src/main.rs
+++ life-cell/firmware/src/main.rs
@@ -11,20 +11,27 @@ use avrxmega_hal::{
 };
 use core::panic::PanicInfo;
 
+const EEPROM_START: usize = 0x1f00;
+const EEPROM_SIZE: usize = 128;
+
 #[avrxmega_hal::entry]
 fn main() -> ! {
     let dp = Peripherals::take().unwrap();
     let pins = Pins::new(dp.PORTA, dp.PORTB);
 
+    let eeprom: &[u8; EEPROM_SIZE] = unsafe { &*(EEPROM_START as *const [u8; EEPROM_SIZE]) };
+
+    // Order matters for the index calculation -
+    // Starting top-left and going around clockwise:
     let neighbors: [Pin<Input<PullUp>>; 8] = [
-        pins.pa4.into_pull_up_input().downgrade(),
-        pins.pa5.into_pull_up_input().downgrade(),
-        pins.pa6.into_pull_up_input().downgrade(),
-        pins.pa7.into_pull_up_input().downgrade(),
-        pins.pb0.into_pull_up_input().downgrade(),
-        pins.pb1.into_pull_up_input().downgrade(),
-        pins.pb2.into_pull_up_input().downgrade(),
-        pins.pb3.into_pull_up_input().downgrade(),
+        pins.pa7.into_pull_up_input().downgrade(), // DIA1
+        pins.pa6.into_pull_up_input().downgrade(), // ADJ1
+        pins.pa4.into_pull_up_input().downgrade(), // DIA2
+        pins.pa5.into_pull_up_input().downgrade(), // ADJ2
+        pins.pb0.into_pull_up_input().downgrade(), // DIA3
+        pins.pb1.into_pull_up_input().downgrade(), // ADJ3
+        pins.pb2.into_pull_up_input().downgrade(), // DIA4
+        pins.pb3.into_pull_up_input().downgrade(), // ADJ4
     ];
     let clk = pins.pa1.into_pull_up_input();
     let mut state = pins.pa2.into_opendrain_high();
@@ -33,15 +40,20 @@ fn main() -> ! {
         //TODO interrupt-based sleep/wake
         while clk.is_high() {}
 
-        let alive_neighbors: u8 = neighbors.iter().map(|pin| pin.is_low() as u8).sum();
+        let mut alive_neighbors: u8 = 0;
+        for neighbor in &neighbors {
+            alive_neighbors <<= 1;
+            alive_neighbors |= neighbor.is_low() as u8;
+        }
+
+        let index = usize::from_le_bytes([alive_neighbors, alive as u8]);
 
         while clk.is_low() {}
 
-        if alive {
-            alive = alive_neighbors == 2 || alive_neighbors == 3;
-        } else {
-            alive = alive_neighbors == 3;
-        }
+        let byte = index / 8;
+        let bit = index % 8;
+        alive = (eeprom[byte] & (1 << bit)) != 0;
+
         if alive {
             state.set_low();
         } else {
